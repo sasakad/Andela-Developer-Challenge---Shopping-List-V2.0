9b69d04648da19bc3da9f3fb0a4d4063856e0e65
David Parseen <david.parseen@gmail.com>
2017-08-29 14:10:34 +0300

    [Feature#[2098487] Added routes to home, signup, dashboard, details, login respectively

---
+++ app/views.py
@@ -0,0 +1,22 @@
+# views.py
+
+from flask import render_template
+
+from app import app
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/signup')
+def signup():
+   return render_template("signup.html")
+@app.route('/dasboard')
+def dasboard():
+   return render_template("dashboard.html")
+@app.route('/dashboard/details')
+def details():
+   return render_template("details.html")
+@app.route('/login')
+def about():
+    return render_template("login.html")
