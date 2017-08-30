df922319a0bc8619a0735d181ac4a909c9ede407
David Parseen <david.parseen@gmail.com>
2017-08-30 09:21:33 +0300

    [Test#[2098487] Added tets for the create() method sucessful, invalid characters and already existing list

--- tests/lists_test.py
+++ tests/lists_test.py
@@ -10,6 +10,22 @@ class TestCasesShoppingList(unittest.TestCase):
 
     def tearDown(self):
         del self._list
+    
+    def test_sucessful_create(self):
+        msg = self._list.create( "dalton", 'Party')
+        self.assertEqual(
+            msg, [{'user': 'dalton', 'name': 'Party'}])
+    
+    def test_for_invalid_characters(self):
+
+        msg = self._list.create('kate', "Back.to-School")
+        self.assertEqual(msg, "Invalid characters")
+    
+    def test_already_existing_list(self):
+
+        self._list.list_of_shopping_lists = [{'user': 'dalton', 'name': 'Party'},{'user': 'dalton', 'name': 'Birthday'}]
+        msg = self._list.create( "dalton", "Party")
+        self.assertEqual(msg, "List already exists.")
 
 if __name__ == '__main__':
     unittest.main()
