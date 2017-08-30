861c85249e1c3081451456c9126c727d0d420145
David Parseen <david.parseen@gmail.com>
2017-08-30 08:57:03 +0300

    [Feature#[2098487] Added ShoppingListItems class for managing manipulation of items in a shopping list

---
+++ app/shopping_lists_items.py
@@ -0,0 +1,11 @@
+""" SHOPPING LISTS ITEMS MANAGEMENT """
+from string import ascii_letters
+
+class ShoppingListItems(object):
+    """ CLASS FOR ADDING, EDITING AND DELETING SHOPPING LIST ITEMS """
+
+
+    def __init__(self):
+        self.list_of_shopping_list_items = []
+
+    
