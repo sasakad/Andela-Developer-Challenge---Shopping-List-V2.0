c4eb3ad3c10c36e0548abdaa51e1cb5e9382c71e
David Parseen <david.parseen@gmail.com>
2017-08-30 08:54:41 +0300

    [Feature#[2098487] Added Registration Method

--- app/accounts.py
+++ app/accounts.py
@@ -14,3 +14,22 @@ def login(self, email, pwd):
                 else:
                     return "Invalid email, password combination"
         return "Account not registered, sign up"
+
+    def registration(self, uname, email, pwd, pwd_confirm):
+        """Method for creating new accounts."""
+        dict_for_each_account = {}
+
+        for account in self.list_of_accounts:
+            if email == account['email']:
+                return "Your Account Already Active. Proceed to login"
+        else:
+            if len(pwd) < 6:
+                return "Password is too short"
+            elif pwd == pwd_confirm:
+                dict_for_each_account['uname'] = uname
+                dict_for_each_account['email'] = email
+                dict_for_each_account['pwd'] = pwd
+                self.list_of_accounts.append(dict_for_each_account)
+            else:
+                return "Your passwords should match"
+        return "Your account is now registered please proceed to login"
