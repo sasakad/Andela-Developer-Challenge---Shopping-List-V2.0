3e82d8d298ebb041e68975fe8b37e2de218cb30b
David Parseen <david.parseen@gmail.com>
2017-08-30 09:45:07 +0300

    [Feature#[2098487] Created create method for creating new lists

--- app/shopping_lists.py
+++ app/shopping_lists.py
@@ -7,4 +7,21 @@ class ShoppingLists(object):
     def __init__(self):
         self.list_of_shopping_lists = []
 
+    def create(self, user, list_name):
+        """ METHOD FOR CREATING SHOPPING LIST """
+        # Checking special characters
+        if all(c in ascii_letters+'-' for c in list_name):
+            users_list_of_shopping_lists = \
+            [item for item in self.list_of_shopping_lists if item['user'] == user]
+            for item in users_list_of_shopping_lists:
+                if list_name == item['name']:
+                    return "List already exists."
+            shopping_list_item = {
+                'name': list_name,
+                'user': user,
+            }
+            self.list_of_shopping_lists.append(shopping_list_item)
+        else:
+            return "Invalid characters"
+        return [item for item in self.list_of_shopping_lists if item['user'] == user]
     
