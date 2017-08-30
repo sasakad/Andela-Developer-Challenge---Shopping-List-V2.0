0b0156f81c40dc3176385bc1e38eaaaa43f4b13b
David Parseen <david.parseen@gmail.com>
2017-08-30 09:38:50 +0300

    [Feature#[2098487] Created ShoppingLists class for managing shopping lists

---
+++ app/shopping_lists.py
@@ -0,0 +1,10 @@
+""" SHOPPING LISTS MANAGEMENT """
+from string import ascii_letters
+
+class ShoppingLists(object):
+    """ CLASS FOR CREATING, EDITING AND DELETING SHOPPING LISTS """
+
+    def __init__(self):
+        self.list_of_shopping_lists = []
+
+    
