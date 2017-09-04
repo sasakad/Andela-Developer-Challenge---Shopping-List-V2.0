54d37e53b4d8f769e6370bdd89f099250aa70f46
David Parseen <david.parseen@gmail.com>
2017-09-04 23:08:20 +0300

    [Fix [2098487]] Moved delete button to table and added edit button

--- app/item_table_creator.py
+++ app/item_table_creator.py
@@ -1,16 +1,25 @@
 # import things
-from flask_table import Table, Col, LinkCol
+from flask_table import Table, Col, LinkCol, ButtonCol
 
 # Declare your table
 class ItemTable(Table):
-    #user = Col('Name')
-    name = Col('Item\'s Name')
-    date = Col('Date Created')
-    number = Col('Number of Items')
+    iname = 'name'
+    lname = 'list'
+    name = Col('List\'s Name', allow_sort=False,th_html_attrs={'style':'width: 30%;'} )
+    date = Col('Date Created', th_html_attrs={'style':'width: 30%;'})
+    number = Col('Number of Items', th_html_attrs={'style':'width: 20%;'})
+    edit_button = ButtonCol('Edit', 'edit_item', button_attrs={'class':"btn btn-sm btn-outline-primary"},
+                url_kwargs={'item_name' : iname})
+    del_button = ButtonCol('Delete', 'del_item', button_attrs={'class':"btn btn-sm btn-outline-danger"}, 
+                url_kwargs={'item_name' : iname, 'list_name': lname})
+
 # Get some objects
 class Item(object):
-    def __init__(self, name, date, number):
-        self.name = name
+    def __init__(self, name, date, number, edit_button, del_button):
+        self.lname = name
         self.date = date
         self.number = number
+        self.edit_button = edit_button
+        self.del_button = del_button
+
 
