50eaa1f97315bfe289be45c19702a4a4bc76801c
David Parseen <david.parseen@gmail.com>
2017-09-04 23:09:01 +0300

    [Fix [2098487]] Moved delete button to table and added edit button

--- app/list_table_creator.py
+++ app/list_table_creator.py
@@ -1,16 +1,26 @@
 # import things
-from flask_table import Table, Col, LinkCol
+from flask_table import Table, Col, LinkCol, ButtonCol
 
 # Declare your table
 class ItemTable(Table):
-    #user = Col('Name')
-    name = Col('List\'s Name')
-    date = Col('Date Created')
-    details = LinkCol('List Items', 'details')
+    lname = 'name'
+    id = Col('id', show=False)
+    name = Col('List\'s Name', allow_sort=False,th_html_attrs={'style':'width: 25%;'} )
+    date = Col('Date Created', th_html_attrs={'style':'width: 25%;'})
+    details = LinkCol('List Items', 'details', th_html_attrs={'style':'width: 25%;'},
+                 url_kwargs={'list_name' : lname})
+    edit_button = ButtonCol('Edit', 'edit_list', button_attrs={'class':"btn btn-sm btn-outline-primary"},
+                url_kwargs={'list_name' : lname})
+    del_button = ButtonCol('Delete', 'del_list', button_attrs={'class':"btn btn-sm btn-outline-danger"}, 
+                url_kwargs={'list_name' : lname})
+
 
 # Get some objects
 class Item(object):
-    def __init__(self, name, date, details = None):
-        self.name = name
+    def __init__(self, name, date, details ,edit_button, del_button):
+        self.lname = name
         self.date = date
+        self.details = details
+        self.edit_button = edit_button
+        self.del_button = del_button
 
