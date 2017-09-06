a8be42a4c0b8162745ef09a10e59827ef5f770bb
David Parseen <david.parseen@gmail.com>
2017-09-06 15:44:55 +0300

    [Feature[2098487] changed the button type to be more catchy

--- app/item_table_creator.py
+++ app/item_table_creator.py
@@ -8,9 +8,9 @@ class ItemTable(Table):
     lname = 'list'
     name = Col('List\'s Name', th_html_attrs={'style':'width: 30%;'})
     date = Col('Date Created', th_html_attrs={'style':'width: 30%;'})
-    number = Col('Number of Items', th_html_attrs={'style':'width: 20%;'})
+    number = Col('Number of Items', th_html_attrs={'style':'width: 15%;'})
     del_button = ButtonCol('Delete', 'del_item',
-                           button_attrs={'class':"btn btn-sm btn-outline-danger"},
+                           button_attrs={'class':"btn btn-sm btn-danger"},
                            url_kwargs={'item_name' : iname, 'list_name': lname})
 
 # Get some objects
